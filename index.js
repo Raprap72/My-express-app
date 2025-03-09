818d8a99580c743b471779e8d1b478e1553dceb2
Raprap72 <wisrtalon@gmail.com>
2025-03-09 13:15:32 +0800

    Mana

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const items = ['Apple', 'Banana', 'Orange'];
 
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+// Serve static files from the "public" folder
 app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 app.get('/about', (req, res) => {
     res.send('About Us');
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
@@ -18,3 +30,15 @@ app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
